ac3fe047d4a50402f5534a506ac727fb8c7d62f0
Lee McAlilly <leemcalilly@gmail.com>
2011-04-11 09:41:32 -0400

    this should be the correct s3 environment variables

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,6 @@ require File.expand_path('../application', __FILE__)
 Seniors::Application.initialize!
 
 # Amazon s3
-S3_KEY = "AKIAJFTZA54JRR4SPGHQ"
-S3_SECRET = "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"
-S3_BUCKET = "with_seniors_in_mind"
+ENV['S3_KEY']='AKIAJFTZA54JRR4SPGHQ'
+ENV['S3_SECRET']='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'
+ENV['S3_BUCKET']='with_seniors_in_mind'
