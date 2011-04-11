c423ed8a54dadab7b1c3e65aac060d1153aed668
Lee McAlilly <leemcalilly@gmail.com>
2011-04-10 20:55:33 -0400

    still working on s3

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Seniors::Application.initialize!
+
+# Amazon s3
+config.S3_KEY = "AKIAJFTZA54JRR4SPGHQ"
+config.S3_SECRET = "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"
+config.S3_BUCKET = "with_seniors_in_mind"
+Refinery.s3_backend = true
